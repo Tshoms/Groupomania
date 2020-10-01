43c86c6163b04ec151a0a68d133197360fe648c4
Tshoms <tshomsjordy@gmail.com>
2020-10-02 00:47:55 +0200

    connection db

--- backend/app.js
+++ backend/app.js
@@ -13,6 +13,20 @@ const db = mysql.createConnection({
 
 // connect
 
+con.connect ((err) => {
+    si (err) {
+      console.log ('Erreur de connexion à Db');
+      revenir;
+    }
+    console.log ('Connexion établie');
+  });
+
+  con.end ((err) => {
+    // La connexion se termine normalement
+    // Garantit que toutes les requêtes restantes sont exécutées
+    // Envoie ensuite un paquet de sortie au serveur MySQL.
+  });
+
 const app = express(); // ce qui permet de créer une appli express .
 
 module.exports = app; // ce qui nous permet d'exporter notre app dans d'autre fichiers .
