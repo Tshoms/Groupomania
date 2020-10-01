d8935bcd50d0309cbd2b220d1684cc4c67f12e56
Tshoms <tshomsjordy@gmail.com>
2020-10-02 00:35:48 +0200

    import mysql

--- backend/app.js
+++ backend/app.js
@@ -1,4 +1,17 @@
 const express = require('express'); // importter 'express.js' .
+const mysql   = require('mysql'); // importer "Mysql" .
+
+// connection base de donnée sql.
+const db = mysql.createConnection({
+
+    host       : 'localhost',
+    user       : 'root',
+    passwords  : '1234567',
+    database   : 'nodemysql'
+
+});
+
+// connect
 
 const app = express(); // ce qui permet de créer une appli express .
 
